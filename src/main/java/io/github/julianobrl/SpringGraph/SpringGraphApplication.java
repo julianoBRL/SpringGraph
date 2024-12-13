014407c104e30bf06192fad0b91f7dd92fe5bfdf
Juliano Lira <juliano0forum@gmail.com>
2024-12-13 14:52:55 -0300

    unido servelet e initializer

--- src/main/java/io/github/julianobrl/SpringGraph/SpringGraphApplication.java
+++ src/main/java/io/github/julianobrl/SpringGraph/SpringGraphApplication.java
@@ -2,9 +2,16 @@ package io.github.julianobrl.SpringGraph;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringGraphApplication {
+public class SpringGraphApplication  extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringGraphApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringGraphApplication.class, args);
