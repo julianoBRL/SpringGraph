8e3e1d6fd15c30d44cbe45d24d422b18c5900f11
Juliano Lira <juliano0forum@gmail.com>
2024-12-13 14:47:59 -0300

    Initial Commit

---
+++ src/main/java/io/github/julianobrl/SpringGraph/SpringGraphApplication.java
@@ -0,0 +1,13 @@
+package io.github.julianobrl.SpringGraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGraphApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/julianobrl/SpringGraph/SpringGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.julianobrl.SpringGraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
